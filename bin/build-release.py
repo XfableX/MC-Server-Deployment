e11a5a79fdb09dfabf181a2504b203d9cc9ba644
XfableX <lachlanmaxwell@icloud.com>
2023-07-05 22:07:17 +1000

    init

---
+++ bin/build-release.py
@@ -0,0 +1,45 @@
+import boto3
+import argparse
+import os
+import yaml
+import tarfile
+import glob
+
+parser = argparse.ArgumentParser(description="Create a release for MC")
+parser.add_argument("-r", "--release-Name", required=True)
+
+args = parser.parse_args()
+
+release_bucket = "fablemc-artifacts"
+release_prefix = os.path.join("MC-Server-deployment/",args.release_name)
+
+session = boto3.Session()
+s3 = session.client("s3")
+cfn = session.client("cloudformation")
+
+def s3_put_object(key, body):
+    payload = {
+        "Bucket": release_bucket,
+        "Key": key,
+        "Body": body
+    }
+    s3.put_object(**payload)
+
+
+artifact_filename = "ansible_bootstrap.tar.gz"
+local_artifact_filename = os.path.join("/tmp/",artifact_filename)
+
+with tarfile.open(local_artifact_filename, "w:gz") as artifact:
+    artifact.add('../Ansible')
+
+s3_put_object(os.path.join(release_prefix,artifact_filename), local_artifact_filename)
+
+bin_filename = "bin.tar.gz"
+local_bin_filename = os.path.join("/tmp/",bin_filename)
+
+with tarfile.open(local_bin_filename, "w:gz") as artifact:
+    artifact.add('../bin')
+s3_put_object(os.path.join(release_prefix, local_bin_filename))
+
+for template in glob.glob("../Cloudformation", "*"):
+    s3_put_object(os.path.join(release_prefix, "templates", os.path.basename(template)), open(template, "rb"))
---
+++ bin/deploy-release.py
@@ -0,0 +1,73 @@
+import boto3
+import argparse
+import os
+import yaml
+import tarfile
+import threading
+
+from botocore.exceptions import ClientError
+
+parser = argparse.ArgumentParser(description="Create a release for MC")
+parser.add_argument("-r", "--release-Name", required=True)
+parser.add_argument("-a", "--ami-id", required=True)
+parser.add_argument("-t", "--template", required=True)
+
+args = parser.parse_args()
+
+release_bucket = "fablemc-artifacts"
+release_prefix = os.path.join("MC-Server-deployment/",args.release_name)
+s3_release_url = "s3://{0}/{1}".format(release_bucket, release_prefix)
+
+template_vars = {
+    "AMI": args.ami_id,
+    "release": args.release_name
+}
+
+session = boto3.Session()
+s3 = session.client("s3")
+cfn = session.client("cloudformation")
+
+def stack_exists(stack_name):
+    paginator = cfn.get_paginator("list_stacks")
+
+    for page in paginator.paginate():
+        for stack in page["StackSummaries"]:
+            if stack["StackStatus"] == 'DELETE_COMPLETE':
+                continue
+            if stack_name == stack["StackName"]:
+                return True
+    return False
+def deploy_stack(cf_stack_name, s3_url):
+    cf_stack_template_s3_url = "{0}/templates/{1}".format(s3_release_url, cf_stack_name)
+
+    cf_payload = {
+        "Stackname":cf_stack_name,
+        "TemplateURL": cf_stack_template_s3_url,
+        "Parameters": template_vars,
+    }
+
+    try:
+        if stack_exists(cf_stack_name):
+            print("INFO: UPDATING {0}".format(cf_stack_name))
+
+            try:
+                stack_result = cfn.update_stack(**cf_payload)
+
+            except ClientError as e:
+                if "No updates are to be performed" in str(e):
+                    print("No updates for {0}".format(cf_stack_name))
+                    return
+                else:
+                    raise e
+            waiter = cfn.get_waiter("stack_update_complete")
+        else:
+            print("INFO: Creating {0}".format(cf_stack_name))
+            stack_result = cfn.create_stack(**cf_payload)
+            waiter = cfn.get_waiter("stack_create_complete")
+        print("Waiting for {0} to be ready".format(cf_stack_name))
+        waiter.wait(stackname=cf_stack_name)
+    except Exception as e:
+        thread_errors.set()
+        raise e
+    
+deploy_stack(args.template, s3_release_url)
