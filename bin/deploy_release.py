1d5d27acbfb245d9634145f900a65c5fdce4897c
XfableX <lachlanmaxwell@icloud.com>
2023-07-06 01:52:33 +1000

    Update deploy_release.py

--- bin/deploy_release.py
+++ bin/deploy_release.py
@@ -14,7 +14,7 @@ parser.add_argument("-t", "--template", required=True)
 
 args = parser.parse_args()
 
-release_bucket = "fablemc-artifacts"
+release_bucket = "arn:aws:s3:ap-southeast-2:453112952343:accesspoint/boto3-artifact"
 release_prefix = os.path.join("MC-Server-deployment/",args.release_name)
 s3_release_url = "s3://{0}/{1}".format(release_bucket, release_prefix)
 
